d4c1bd7f0155133aa25b666fe0e703c778753efc
Kateryna Zhavoronok <Kateryna.Zhavoronok@aprimo.com>
2019-03-11 10:53:21 +0200

    add pure component

--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const HelloWorldSimple = () => (<div>Hello, world! (pure)</div>);
+const HelloWorldSimple = () => (<div>Hello, world! (func)</div>);
 
-function HelloWorldFunc() {
-    // Import result is the URL of your image
-    return <div>Hello, world! (functional component)</div>;
+class HelloPure extends React.PureComponent {
+  render() {
+    return <div>
+              Hello, World! (pure component)
+          </div>;
   }
+}
 
 var HelloWorldElement =  React.createElement("div", null, "Hello, World! (element)");
 
 class Hello extends React.Component {
     render() {
       return <div>
+                <HelloPure /> 
                 Hello, World! (component)
                 <HelloWorldSimple /> 
                 {HelloWorldElement}
-                {HelloWorldFunc()} 
-            </div>;
+             </div>;
     }
   }
 
